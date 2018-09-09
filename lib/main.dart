b4f1e2e54d5db7f8bdda48713e5a432390a1ee91
尹彦军 <yinyanjun@yinyanjundeMacBook-Pro.local>
2018-09-10 00:02:29 +0800

    fab animation add source link

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// from https://medium.com/@agungsurya/create-a-simple-animated-floatingactionbutton-in-flutter-2d24f37cfbcc
 import 'package:flutter/material.dart';
 import 'package:flutter_animations/basic.dart';
 import 'package:flutter_animations/fab.dart';

3c0b1ed0683683f1d1ea6ece6dd1bc0469b3eb5a
尹彦军 <yinyanjun@yinyanjundeMacBook-Pro.local>
2018-09-10 17:31:54 +0800

    basic flashing the text

--- lib/basic.dart
+++ lib/basic.dart
@@ -5,10 +5,60 @@ class Basic extends StatefulWidget {
   State<StatefulWidget> createState() => _BasicState();
 }
 
-class _BasicState extends State<Basic> {
+class _BasicState extends State<Basic> with TickerProviderStateMixin{
+  AnimationController _opacityController;
+  Animation<double> _opacity;
+  bool _showHint = false;
+
+  @override
+  void initState() {
+    _opacityController = AnimationController(vsync: this, duration: Duration(milliseconds: 800));
+    _opacity = CurvedAnimation(parent: _opacityController, curve: Curves.easeInOut)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          _opacityController.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          _opacityController.forward();
+        }
+      });
+    _opacityController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _opacityController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Basic');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Basic animation'),
+      ),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            !_showHint ? FadeTransition(
+              opacity: _opacity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.arrow_back),
+                  Padding(padding: EdgeInsets.only(left: 8.0)),
+                  Text('Swipe it', style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.blueGrey
+                  ),)
+                ],
+              ),
+            ) : Container()
+          ],
+        ),
+      ),
+    );
   }
 
 }
